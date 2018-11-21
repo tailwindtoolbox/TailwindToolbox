b374f5cffc0157a4e7d5cb198d1de56ef59d0116
tailwindtoolbox <support@tailwindtoolbox.com>
2018-11-21 22:31:08 +0000

    Added Gatsby starter template

--- dist/starter-templates.php
+++ dist/starter-templates.php
@@ -204,6 +204,27 @@
 							</div>
 						</a>
 					</div>
+
+
+					<!--Gatsby Template-->
+					<div class="w-full md:w-1/3 mb-12 pb-3 sm:pb-0">
+						<a href="https://github.com/taylorbryant/gatsby-starter-tailwind" class="no-underline hover:no-underline">
+							<div class="bg-white rounded overflow-hidden shadow mx-5 sm:mx-2 hover:shadow-lg">
+								<div class="p-5 text-center">
+									<img src="templates/gatsby-tailwind.png" class="h-full w-4/5 md:w-full rounded shadow">
+									<div class="font-bold text-xl mt-3 mb-1">Gatsby Starter Template</div>
+									
+									<p class="text-grey-darker text-base mb-2">
+										A Gatsby.js starter project template using Tailwind CSS
+									</p>
+									<p class="text-grey-darker text-base mb-5"><a class="no-underline" href="https://github.com/taylorbryant" target="_blank" rel="noopener"><span><svg class="h-4 fill-current text-teal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/></svg></span> Taylor Bryant / @taylorbryant</a></p>
+
+									<a href="https://github.com/taylorbryant/gatsby-starter-tailwind" target="_blank" rel="noopener"><button class="block w-full bg-brand hover:bg-teal text-white text-sm border-none rounded font-bold fixed p-3 mt-2 relative pin-x pin-b"><svg class="fill-current h-3 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"/></svg> Visit Template</button></a>
+											
+								</div>
+							</div>
+						</a>
+					</div>
 					
 
 			
