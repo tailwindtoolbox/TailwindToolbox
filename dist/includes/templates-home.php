bb207bd571aec1b2cd78aae5484ffabfa4629972
tailwindtoolbox <support@tailwindtoolbox.com>
2019-04-08 21:55:38 +0100

    updated home

--- dist/includes/templates-home.php
+++ dist/includes/templates-home.php
@@ -1,51 +1,45 @@
 <!--section-->
 <div class="container mx-auto mb-16" id="html-templates">
-		<div class="w-full px-0">
-		
-			<!--<a name="HTML"></a>
-			<div class="bg-transparent rounded overflow-hidden mx-1 text-center pt-6">
-			
-			<h1 class="text-brand pt-12"><span class="bg-brand font-bold text-center text-white text-3xl sm:text-4xl md:text-5xl px-3 mb-5 sm:mb-16" style="box-decoration-break: clone;-webkit-box-decoration-break: clone;"><span>HTML Starter Templates</span></span></h1>
-			<p class="p-6 nunito text-grey-darkest">These templates are not finished articles, they are just a starting point skeleton template using the default Tailwind CSS styling (served via the CDN) - which you can turn into your own creation.</p>
--->		
+		<div class="w-full px-5 md:px-0">
+	
 
 			<div class="flex flex-wrap items-stretch mb-2 md:px-3">				
 				
-				<!--Skeleton Template-->
-				<div class="w-full md:w-1/3 mb-12  pb-3 sm:pb-0">
+				<!--Skeleton Template-->		
+				<div class="w-full md:w-1/2 lg:w-1/3 mb-4 md:mb-12 pb-3 sm:pb-0" data-twcat="{{category}}">
 					<a href="https://www.tailwindtoolbox.com/skeleton" class="no-underline hover:no-underline">
-						<div class="bg-white rounded overflow-hidden shadow mx-5 sm:mx-2 hover:shadow-lg">
-							<div class="p-5 text-center">
-								<img src="skeleton.png" class="h-full w-4/5 md:w-full rounded shadow">
-								<div class="font-bold text-xl my-3">Skeleton Template</div>
-								<p class="text-grey-darker text-base mb-5">
+						<div class="bg-white rounded overflow-hidden shadow mx-1 sm:mx-2 hover:shadow-lg">
+							<img src="skeleton.png" class="h-full w-full rounded shadow">
+							<div class="p-2 lg:px-5 lg:pt-0 mb-2 text-center">
+								<div class="font-bold text-xl pt-2 my-1">Skeleton Template</div>
+								<div class="mx-auto border-t border-grey-light w-12 pt-2"></div>
+								<p class="text-grey-darker text-sm mb-5 whitespace-normal">
 									Simple skeleton/boilerplate template to copy/paste into your editor
 								</p>
-								<div>
-									<a href="https://www.tailwindtoolbox.com/skeleton"><button class="block w-full bg-brand hover:bg-teal text-white text-sm border-none rounded font-bold fixed p-3 mt-2 relative pin-x pin-b"><svg class="inline fill-current h-3 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg> Download</button></a>
-								</div>
+
+								<a href="https://www.tailwindtoolbox.com/skeleton" target="_blank" rel="noopener"><button class="block w-full bg-brand hover:bg-teal text-white text-sm border-none rounded font-bold fixed p-3 mt-2 relative pin-x pin-b"><svg class="inline fill-current h-3 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg> Download</button></a>
 								
 							</div>
 						</div>
 					</a>
 				</div>
-				
 
 				<?php
 				// Store Card template
 				$card ='
 				<!--{{title}} Template-->
-				<div class="w-full md:w-1/3 mb-12 pb-3 sm:pb-0" data-twcat="{{category}}">
+				<div class="w-full md:w-1/2 lg:w-1/3 mb-4 md:mb-12 pb-3 sm:pb-0" data-twcat="{{category}}">
 					<a href="{{url}}" class="no-underline hover:no-underline">
-						<div class="bg-white rounded overflow-hidden shadow mx-5 sm:mx-2 hover:shadow-lg">
-							<div class="p-5 text-center">
-								<img src="{{url_image}}" class="h-full w-3/5 md:w-full rounded shadow">
-								<div class="font-bold text-xl my-3">{{title}}</div>
-								<p class="text-grey-darker text-base mb-5">
+						<div class="bg-white rounded overflow-hidden shadow mx-1 sm:mx-2 hover:shadow-lg">
+							<img src="{{url_image}}" class="h-full w-full rounded shadow">
+							<div class="p-2 lg:px-5 lg:pt-0 mb-2 text-center">
+								<div class="font-bold text-xl pt-2 my-1">{{title}}</div>
+								<div class="mx-auto border-t border-grey-light w-12 pt-2"></div>
+								<p class="text-grey-darker text-sm mb-5 whitespace-normal">
 									{{description}}
 								</p>
 
-								<a href="{{download}}" target="_blank" rel="noopener"><button class="block w-full bg-brand hover:bg-teal text-white text-sm border-none rounded font-bold fixed p-3 mt-2 relative pin-x pin-b"><svg class="inline fill-current h-3 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg> Download</button></a>
+								<a href="{{url}}" target="_blank" rel="noopener"><button class="block w-full bg-brand hover:bg-teal text-white text-sm border-none rounded font-bold fixed p-3 mt-2 relative pin-x pin-b"><svg class="inline fill-current h-3 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg> Download</button></a>
 								
 							</div>
 						</div>
