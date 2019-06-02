aaf8c291f983fa9d5fcca7c1da96f6b8c4ad329e
tailwindtoolbox <support@tailwindtoolbox.com>
2019-06-02 06:55:17 +0100

    Fixed menu

--- dist/index.php
+++ dist/index.php
@@ -83,6 +83,87 @@ function showMeThe(thing) {
 		elmnt.scrollIntoView();
 }
 
+
+	/*Toggle dropdown list*/
+	/*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
+
+	var navMenuDiv = document.getElementById("nav-content");
+	var navMenu = document.getElementById("nav-toggle");
+	
+	document.onclick = check;
+
+	function check(e){
+	  var target = (e && e.target) || (event && event.srcElement);
+
+	  //Nav Menu
+	  if (!checkParent(target, navMenuDiv)) {
+		// click NOT on the menu
+		if (checkParent(target, navMenu)) {
+		  // click on the link
+		  if (navMenuDiv.classList.contains("hidden")) {
+			navMenuDiv.classList.remove("hidden");
+		  } else {navMenuDiv.classList.add("hidden");}
+		} else {
+		  // click both outside link and outside menu, hide menu
+		  navMenuDiv.classList.add("hidden");
+		}
+	  }
+	  
+	}
+
+	function checkParent(t, elm) {
+	  while(t.parentNode) {
+		if( t == elm ) {return true;}
+		t = t.parentNode;
+	  }
+	  return false;
+	}
+
+
+
+function filterTemplates(filterVal) {
+	//Get all the templates
+	var divs = document.querySelectorAll("[data-twcat]");
+
+	//Loop through and show (block) all which match the criteira and hide (none) the rest
+	for (var i = 0; i < divs.length; ++i) {
+		if (divs[i].dataset.twcat.indexOf(filterVal) >= 0) { //includes(filterVal)) {
+			divs[i].style.display='block';
+		} else {
+			divs[i].style.display='none';
+		}
+	}
+	
+	//Reset the all filter to be "all" instead of ""
+	if (filterVal == '') filterVal = "all";
+	
+
+
+	//Get all the filter buttons
+	var btns = document.querySelectorAll("[data-twfilter]");
+	var filterMsg = document.getElementById('filterMsg');
+
+	//Loop through and set the criteria filter button to active
+	for (var i = 0; i < btns.length; ++i) {
+		if (btns[i].dataset.twfilter == filterVal) {
+			btns[i].classList.add('active-tab');
+		} else {
+			btns[i].classList.remove('active-tab');
+		}
+	}
+	
+	//Hide message if showing all
+	if (filterVal == "all") {
+		filterMsg.classList.add('invisible');
+	} else {
+		filterMsg.classList.remove('invisible');
+		filterMsg.innerHTML = 'Showing: ' + filterVal + " templates - Click here to show all templates!";
+	}
+
+}
+
 </script>
+
+
 </body>
 </html>
