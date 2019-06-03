b7c2fe41264a6bec039fc9f94556abef9c13cfe9
Tailwind Toolbox <43570497+tailwindtoolbox@users.noreply.github.com>
2019-06-03 16:34:17 +0100

    Add files via upload

---
+++ dist/components/centered-page-code.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Centered Page</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css"> 
+  <!--Replace with your tailwind.css once created-->
+
+</head>
+
+<body class="bg-gray-100">
+
+  <div class="flex items-center justify-center h-screen">
+    
+	<div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+		Centered Content
+	</div>
+	
+  </div>
+
+
+</body>
+
+</html>
