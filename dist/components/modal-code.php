b0dd9790a41706eefbe0e0583ff061185bae8535
Tailwind Toolbox <43570497+tailwindtoolbox@users.noreply.github.com>
2019-06-18 11:05:29 +0100

    Update modal-code.php

--- dist/components/modal-code.php
+++ dist/components/modal-code.php
@@ -30,7 +30,9 @@
 
         <div class="modal-title flex justify-between items-center pb-2">
           <p class="text-2xl font-bold">Simple Modal!</p>
-          <div class="modal-close cursor-pointer font-bold text-3xl">x</div>
+	  <div class="modal-close cursor-pointer">
+	    <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path></svg>
+	  </div>
         </div>
 		
         <div class="modal-body py-4 text-left">
