48de8d3a31a1df37209d91086e1370c1890ed2c9
tailwindtoolbox <support@tailwindtoolbox.com>
2019-06-26 22:55:30 +0100

    Removed author from home page components

--- dist/includes/components-home.php
+++ dist/includes/components-home.php
@@ -24,8 +24,7 @@
 							<p class="card-description">
 								{{description}}
 							</p>
-							<p class="card-author"><a class="no-underline" href="{{url_author}}" target="_blank" rel="noopener"><svg class="inline h-2 pr-2 fill-current text-teal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/></svg>{{author}}</a></p>
-
+							
 						</div>
 					</a>
 				</div>
