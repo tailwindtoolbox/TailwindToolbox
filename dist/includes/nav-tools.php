35c864a13cfa026d2266632b91660cd19aa2ce11
tailwindtoolbox <support@tailwindtoolbox.com>
2019-09-28 00:07:34 +0100

    Fixed header flex

--- dist/includes/nav-tools.php
+++ dist/includes/nav-tools.php
@@ -4,7 +4,7 @@
 	
 
 		<!--Logo-->
-		<div class="flex-1 pl-4 lg:pl-0">
+		<div class="w-full md:w-1/2 pl-4 lg:pl-0 pb-3 md:pb-0">
 			<a class="font-extrabold text-white no-underline hover:text-white hover:no-underline" href="https://www.tailwindtoolbox.com">
 				<svg class="inline fill-current text-white h-4 md:h-6 mb-2 md:mb-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 3H2v14h5V3zm2 0v14h9V3H9zM0 3c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm3 1h3v2H3V4zm0 3h3v2H3V7zm0 3h3v2H3v-2z"></path></svg> 
 				<span class="text-xl lg:text-2xl xl:text-3xl pl-2">Tailwind Toolbox</span>
@@ -15,7 +15,7 @@
 	
 		
 		<!--Search-->
-		<div class="flex-1 w-full max-w-md mx-auto order-3">
+		<div class="w-full md:w-1/2 max-w-md mx-auto md:ml-auto order-3">
 			
 			<div class="relative max-w-3xl mx-auto px-6">
 				<div class="absolute h-10 mt-1 left-0 top-0 flex items-center pl-10">
@@ -30,7 +30,7 @@
 		<!--End Search-->
 
 		<!--Filters-->
-		<div class="w-full bg-brand-white shadow-md scroll" >
+		<div class="w-full bg-brand-white shadow-md scroll inset-x-0 bottom-0 md:relative" >
 			<ul class="container mx-auto list-reset flex border-b text-sm md:text-base">
 
 				<li class="mr-1">
