40414b27b69a8d59bf31c02134210b0b31b9aa33
tailwindtoolbox <support@tailwindtoolbox.com>
2019-12-17 23:08:15 +0000

    Guides WIP

---
+++ dist/guides/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Tailwind Toolbox - Beginner Guides for Tailwind CSS</title>
+	<meta name="description" content="Beginner guides for getting started with Tailwind CSS">
+	<meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,beginner guides, getting started with tailwind css, tldr">
+	<?php include '../includes/head-guides.php';?>
+	<?php include '../includes/analytics.php';?>
+</head>
+<body class="bg-brand-white leading-normal tracking-normal nunito">
+
+	<?php include '../includes/nav.php';?>
+
+	<!--header-->
+	<div class="z-20 mt-24 pb-6 mb-6 flex items-center text-center" style="background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);">	
+		<div class="flex flex-col w-full justify-center items-center pt-6 md:pt-16">
+			<div class="px-3">
+				<h1 class="pt-6">
+					<span class="bg-brand font-bold text-center text-white text-3xl sm:text-4xl px-3 mb-5 sm:mb-16" style="box-decoration-break: clone;-webkit-box-decoration-break: clone;"><span>Tailwind Beginner Guides</span></span>
+				</h1>
+				<p class="max-w-3xl mx-auto leading-normal my-6 font-bold text-base lg:text-xl text-left lg:text-center">Some useful tools and utilities to help you setup your development environment and become more familiar with Tailwind CSS</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container mx-auto mb-8 flex inline-block flex-wrap">
+		<div class="w-full pb-6 md:w-1/5">
+			<div class="sticky pin-t flex flex-col" style="top:9em;">
+				<!--Filters-->
+				<div class="w-full bg-brand-white pl-3 pt-6 md: pl-0 md:pt-0 order-2 md:order-1">
+					<p class="text-brand text-center font-bold mb-2">Filters</p>
+					<ul class="container scroll mx-auto list-reset flex flex-row md:flex-col border-b md:pb-4 text-sm md:text-base">
+						<li class="mr-1">
+							<button class="tab -mb-px active-tab" onclick="javascript:filterTemplates('');" data-twfilter="all">All</button>
+						</li>
+						<?php 
+							$filters = array('IDE', 'Generator', 'Design', 'Help');
+							foreach($filters as $filter) {
+								echo "\t\t\t\t\t\t<li class=\"mr-1\">\n";
+								echo "\t\t\t\t\t\t\t<button class=\"tab -mb-px\" onclick=\"javascript:filterTemplates('" . $filter . "');\" data-twfilter=\"" . $filter . "\">" . $filter . "</button>\n";
+								echo "\t\t\t\t\t\t</li>\n";
+							}
+						?>		
+					</ul>
+				</div>
+				<!--/filters-->
+				<div class="order-1 md:order-2">
+					<p class="text-brand text-center font-bold mb-2 md:pt-3">Advertisment</p>
+					<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7D52JJ&placement=wwwtailwindtoolboxcom" id="_carbonads_js"></script>
+				</div>
+			</div>
+		
+		</div>
+
+		<div class="w-full md:w-4/5 px-0">
+			<?php 
+				$show_section = "Guide";
+				$show_home = false;
+				include '../includes/render-guides.php';
+			?>
+		</div>
+
+	</div>
+
+	<?php include '../includes/footer-templates.php';?>
+
+	<?php include '../includes/js.php';?>
+
+
+</body>
+</html>
---
+++ dist/includes/head-guides.php
@@ -0,0 +1,32 @@
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700,800" rel="stylesheet">
+	<link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+	<link rel="manifest" href="../site.webmanifest">
+	<link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#00aba9">
+	<meta name="theme-color" content="#3b7977">
+
+	<!-- Primary Meta Tags -->
+	<title>Tailwind Toolbox - Beginner guides for Tailwind CSS</title>
+	<meta name="title" content="Tailwind Toolbox - Beginner guides for Tailwind CSS">
+	<meta name="description" content="Beginner guides to help you get started quickly to creating websites in Tailwind CSS!">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://www.tailwindtoolbox.com/">
+	<meta property="og:title" content="Tailwind Toolbox - Beginner guides for Tailwind CSS">
+	<meta property="og:description" content="Beginner guides to help you get started quickly to creating websites in Tailwind CSS!">
+	<meta property="og:image" content="https://www.tailwindtoolbox.com/social.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://www.tailwindtoolbox.com/">
+	<meta property="twitter:title" content="Tailwind Toolbox - Beginner guides for Tailwind CSS">
+	<meta property="twitter:description" content="Beginner guides to help you get started quickly to creating websites in Tailwind CSS!">
+	<meta property="twitter:image" content="https://www.tailwindtoolbox.com/social.png">
+
+	<!-- Stylesheets -->
+	<link href="../css/tailwind.min.css?v=20191019" rel="stylesheet">
+	<link href="../css/carbon.min.css?v=20191019" rel="stylesheet">
