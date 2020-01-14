24ea4d66cc3b94a899d15f03a9579389fcdf4dbc
Tailwind Toolbox <43570497+tailwindtoolbox@users.noreply.github.com>
2020-01-14 21:38:43 +0000

    Updated to new SA script

--- dist/includes/analytics.php
+++ dist/includes/analytics.php
@@ -1,2 +1,2 @@
-	<script async defer src="https://api.tailwindtoolbox.com/app.js"></script> 
+	<script async defer src="https://api.tailwindtoolbox.com/latest.js"></script> 
 	<noscript><img src="https://api.tailwindtoolbox.com/image.gif" alt=""></noscript>
