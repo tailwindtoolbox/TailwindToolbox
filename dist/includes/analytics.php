df9c623c13ea34a37bbca4220b6f12ba2a04c11e
Tailwind Toolbox <43570497+tailwindtoolbox@users.noreply.github.com>
2020-03-02 08:45:07 +0000

    Removed SA test script

--- dist/includes/analytics.php
+++ dist/includes/analytics.php
@@ -1,3 +1,2 @@
-	<script async src="https://api.tailwindtoolbox.com/temp-new.js" data-hostname="beta.api.tailwindtoolbox.com"></script> 	
-	<script async defer src="https://api.tailwindtoolbox.com/latest.js"></script> 
+        <script async defer src="https://api.tailwindtoolbox.com/latest.js"></script> 
 	<noscript><img src="https://api.tailwindtoolbox.com/image.gif" alt=""></noscript>
