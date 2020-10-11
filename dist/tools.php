2111261cfa5e397be7a60979a803f554f3032b34
tailwindtoolbox <support@tailwindtoolbox.com>
2020-10-11 21:17:33 +0100

    Added a few changes

--- dist/tools.php
+++ dist/tools.php
@@ -39,7 +39,7 @@
 							<button class="tab -mb-px active-tab" onclick="javascript:filterTemplates('');" data-twfilter="all">All</button>
 						</li>
 						<?php
-						$filters = array('IDE', 'Design', 'Help');
+						$filters = array('Browser', 'IDE', 'Design', 'Help');
 						foreach ($filters as $filter)
 						{
 							echo "\t\t\t\t\t\t<li class=\"mr-1\">\n";
