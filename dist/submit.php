3162de317771317419224658f9927993b7035fe3
tailwindtoolbox <support@tailwindtoolbox.com>
2020-12-28 16:15:44 +0000

    Added generator

--- dist/submit.php
+++ dist/submit.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tailwind Toolbox - Submit</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Tailwind Toolbox - Submit</title>
 	<meta name="description" content="Free open source Tailwind CSS starter templates to quickly start your next project">
 	<meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,admin templates, admin template, admin dashboard, free tailwind templates, tailwind example">
-	<?php include 'includes/head.php';?>
-	<?php include 'includes/analytics.php';?>
-		
+	<?php include 'includes/head.php'; ?>
+	<?php include 'includes/analytics.php'; ?>
+
 </head>
+
 <body class="bg-brand-white leading-normal tracking-normal nunito">
 
-	<?php include 'includes/nav.php';?>
+	<?php include 'includes/nav.php'; ?>
 
 	<!--header-->
-	<div class="z-20 mt-24 pb-6 mb-6 flex items-center text-center" style="background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);">	
+	<div class="z-20 mt-24 pb-6 mb-6 flex items-center text-center" style="background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);">
 		<div class="flex flex-col w-full justify-center items-center pt-6 md:pt-16">
 			<div class="px-3">
 				<h1 class="pt-6">
@@ -35,31 +37,35 @@
 					<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7D52JJ&placement=wwwtailwindtoolboxcom" id="_carbonads_js"></script>
 				</div>
 			</div>
-		
+
 		</div>
 
-			<div class="px-4 w-full md:w-3/5">
-			
+		<div class="px-4 w-full md:w-3/5">
+
 			<form id="requestForm">
 
 				<div class="mt-4">
 					<span class="text-gray-700">What would you like to suggest?</span>
 					<div class="mt-2">
 						<label class="inline-flex items-center">
-						<input type="radio" class="form-radio text-brand" name="section" value="Template" checked>
-						<span class="ml-2">Template</span>
+							<input type="radio" class="form-radio text-brand" name="section" value="Template" checked>
+							<span class="ml-2">Template</span>
 						</label>
 						<label class="inline-flex items-center ml-6">
-						<input type="radio" class="form-radio text-brand" name="section" value="Component">
-						<span class="ml-2">Component</span>
+							<input type="radio" class="form-radio text-brand" name="section" value="Component">
+							<span class="ml-2">Component</span>
 						</label>
 						<label class="inline-flex items-center ml-6">
-						<input type="radio" class="form-radio text-brand" name="section" value="Plugin">
-						<span class="ml-2">Plugin</span>
+							<input type="radio" class="form-radio text-brand" name="section" value="Generator">
+							<span class="ml-2">Generator</span>
 						</label>
 						<label class="inline-flex items-center ml-6">
-						<input type="radio" class="form-radio text-brand" name="section" value="Tool">
-						<span class="ml-2">Tool/Utility</span>
+							<input type="radio" class="form-radio text-brand" name="section" value="Tool">
+							<span class="ml-2">Tool/Utility</span>
+						</label>
+						<label class="inline-flex items-center ml-6">
+							<input type="radio" class="form-radio text-brand" name="section" value="Plugin">
+							<span class="ml-2">Plugin</span>
 						</label>
 					</div>
 				</div>
@@ -85,103 +91,106 @@
 					<input id="srcAuthor" name="author" class="form-input mt-1 block w-full" placeholder="Author's Name / GitHub username" onKeyUp="change(this.value,'outputAuthor','Author / @github')">
 				</label>
 
-				<button onclick="postStuff()" type="button" name="submit_btn" class="mt-6 w-full max-w-xs flex items-center justify-center bg-brand hover:bg-teal-500 outline-none focus:border-teal-900 text-white font-extrabold py-3 px-5 rounded shadow"><svg class="inline fill-current h-4 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"></path></svg> Submit</button>
+				<button onclick="postStuff()" type="button" name="submit_btn" class="mt-6 w-full max-w-xs flex items-center justify-center bg-brand hover:bg-teal-500 outline-none focus:border-teal-900 text-white font-extrabold py-3 px-5 rounded shadow"><svg class="inline fill-current h-4 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+						<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"></path>
+					</svg> Submit</button>
 
 
-				</form>
-				<div id="status" class="py-3 font-bold"></div>
+			</form>
+			<div id="status" class="py-3 font-bold"></div>
+		</div>
+
+		<div class="mx-auto w-1/2 md:w-1/5">
+			<p class="text-brand text-center font-bold mb-2 md:pt-3">Preview</p>
+
+			<div>
+				<div class="card">
+					<div class="relative">
+						<img src="placeholder.png" alt="Accessibility">
+
+					</div>
+
+					<div class="card-title text-brand" id="outputTitle">
+						Add Title
+					</div>
+					<p class="card-description" id="outputDesc">
+						Short Description which appears on hover
+					</p>
+					<p class="card-author text-brand"><svg class="inline h-2 pr-2 fill-current text-teal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+							<path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"></path>
+						</svg><span id="outputAuthor">Author / @github</a></p>
+
+				</div>
+
 			</div>
-
-			<div class="mx-auto w-1/2 md:w-1/5">
-				<p class="text-brand text-center font-bold mb-2 md:pt-3">Preview</p>
-
-					<div>
-					<div class="card">
-							<div class="relative">
-								<img src="placeholder.png" alt="Accessibility">
-								
-							</div>
-
-							<div class="card-title text-brand" id="outputTitle">
-								Add Title
-							</div>
-							<p class="card-description" id="outputDesc">
-								Short Description which appears on hover
-							</p>
-							<p class="card-author text-brand"><svg class="inline h-2 pr-2 fill-current text-teal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"></path></svg><span id="outputAuthor">Author / @github</a></p>
-
-						</div>
-					
-				</div>
-
-				</div>
-
 
 		</div>
 
+
 	</div>
 
-	<?php include 'includes/footer.php';?>
+	</div>
 
-	<?php include 'includes/js.php';?>
+	<?php include 'includes/footer.php'; ?>
 
-<script>
+	<?php include 'includes/js.php'; ?>
 
-function change(content,divtoaffect,defaultText) { 
+	<script>
+		function change(content, divtoaffect, defaultText) {
 
-  if (content === '') {
-	document.getElementById(divtoaffect).innerHTML = defaultText; 
-  } else {
-    document.getElementById(divtoaffect).innerHTML = content; 
-  }
-}
-
-function changeLink(content,divtoaffect,defaultText) { 
-
-if (content === '') {
-  document.getElementById(divtoaffect).href = '#'; 
-} else {
-  document.getElementById(divtoaffect).href = content; 
-}
-}
-
-
-
-function postStuff(){
-  // Create our XMLHttpRequest object
-  var hr = new XMLHttpRequest();
-  // Create some variables we need to send to our PHP file
-  var url = "submit-suggestion.php";
-  var section = document.querySelectorAll("input[name=section]:checked")[0].value;
-  var srcTitle = document.getElementById("srcTitle").value;
-  var srcDesc = document.getElementById("srcDesc").value;
-  var srcUrl = document.getElementById("srcUrl").value;
-  var srcAuthor = document.getElementById("srcAuthor").value;
-  var vars = "section="+section+"&title="+srcTitle+"&desc="+srcDesc+"&url="+srcUrl+"&author="+srcAuthor;
-  
- if (srcTitle != '' && srcDesc !='' && srcUrl != '' && srcAuthor != '') {
-	hr.open("POST", url, true);
-	hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	hr.onreadystatechange = function() {
-		if(hr.readyState == 4 && hr.status == 200) {
-			var return_data = hr.response;
-			document.getElementById("status").innerHTML = return_data;
+			if (content === '') {
+				document.getElementById(divtoaffect).innerHTML = defaultText;
+			} else {
+				document.getElementById(divtoaffect).innerHTML = content;
+			}
 		}
-	}
-	// Send the data to PHP now... and wait for response to update the status div
-	hr.send(vars); // Actually execute the request
-	document.getElementById("status").innerHTML = "processing...";
-	return false;
- } else {
-	 document.getElementById("status").innerHTML = '<p style="color:red;">🚫 Please ensure all fields are completed!</p>';
-	 return false;
- }
+
+		function changeLink(content, divtoaffect, defaultText) {
+
+			if (content === '') {
+				document.getElementById(divtoaffect).href = '#';
+			} else {
+				document.getElementById(divtoaffect).href = content;
+			}
+		}
 
 
-}
 
-</script>
+		function postStuff() {
+			// Create our XMLHttpRequest object
+			var hr = new XMLHttpRequest();
+			// Create some variables we need to send to our PHP file
+			var url = "submit-suggestion.php";
+			var section = document.querySelectorAll("input[name=section]:checked")[0].value;
+			var srcTitle = document.getElementById("srcTitle").value;
+			var srcDesc = document.getElementById("srcDesc").value;
+			var srcUrl = document.getElementById("srcUrl").value;
+			var srcAuthor = document.getElementById("srcAuthor").value;
+			var vars = "section=" + section + "&title=" + srcTitle + "&desc=" + srcDesc + "&url=" + srcUrl + "&author=" + srcAuthor;
+
+			if (srcTitle != '' && srcDesc != '' && srcUrl != '' && srcAuthor != '') {
+				hr.open("POST", url, true);
+				hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+				hr.onreadystatechange = function() {
+					if (hr.readyState == 4 && hr.status == 200) {
+						var return_data = hr.response;
+						document.getElementById("status").innerHTML = return_data;
+					}
+				}
+				// Send the data to PHP now... and wait for response to update the status div
+				hr.send(vars); // Actually execute the request
+				document.getElementById("status").innerHTML = "processing...";
+				return false;
+			} else {
+				document.getElementById("status").innerHTML = '<p style="color:red;">🚫 Please ensure all fields are completed!</p>';
+				return false;
+			}
+
+
+		}
+	</script>
 
 </body>
+
 </html>
