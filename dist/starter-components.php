61a6064e291665c9f7489b0c74f7d45bbde66205
tailwindtoolbox <support@tailwindtoolbox.com>
2021-11-18 21:28:48 +0000

    Added windster

--- dist/starter-components.php
+++ dist/starter-components.php
@@ -40,7 +40,7 @@
 							<button class="tab -mb-px active-tab" onclick="javascript:filterTemplates('');" data-twfilter="all">All</button>
 						</li>
 						<?php
-						$filters = array('HTML', 'Vue', 'Angular');
+						$filters = array('HTML');
 						foreach ($filters as $filter)
 						{
 							echo "\t\t\t\t\t\t<li class=\"mr-1\">";
