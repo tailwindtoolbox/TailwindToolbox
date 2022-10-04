416137e6b4333376cc91b15bc71ce5143de57fd7
tailwindtoolbox <support@tailwindtoolbox.com>
2022-10-04 20:55:52 +0100

    wip: astro

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Tailwind Toolbox";
+export const SITE_DESCRIPTION = "Free open source Tailwind CSS starter templates to get you started quickly to creating websites in Tailwind CSS!!";
