a5de47ce8884b489092ccf21c274141d30d6d5ec
Tailwind Toolbox <43570497+tailwindtoolbox@users.noreply.github.com>
2023-06-20 11:07:45 +0100

    Update head.php

--- dist/includes/head.php
+++ dist/includes/head.php
@@ -24,3 +24,12 @@
 <!-- Stylesheets -->
 <link href="css/tailwind.min.css?v=20210110" rel="stylesheet">
 <link href="css/carbon.min.css?v=20210406" rel="stylesheet">
+
+<!-- Microsoft Clarity -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "hmsd5424xn");
+</script>
