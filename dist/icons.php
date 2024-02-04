e42cc658724abf6f46762b46c266d2941b3d74d5
tailwindtoolbox <support@tailwindtoolbox.com>
2024-02-04 19:22:27 +0000

    add icon name

--- dist/icons.php
+++ dist/icons.php
@@ -6,11 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Tailwind Toolbox - Icon Generator for Tailwind CSS</title>
-	<meta name="description" content="Icon Viewer for Heroicons, Tabler and Feather Icons - set the size and colour for your icon and copy the code for your Tailwind CSS project">
-	<meta name="keywords" content="tailwind,tailwindcss,heroicons, heroicons viewer,tabler, tabler viewer, feather icons, feather viewer">
+	<meta name="description"
+		content="Icon Viewer for Heroicons, Tabler and Feather Icons - set the size and colour for your icon and copy the code for your Tailwind CSS project">
+	<meta name="keywords"
+		content="tailwind,tailwindcss,heroicons, heroicons viewer,tabler, tabler viewer, feather icons, feather viewer">
 	<?php include 'includes/head.php'; ?>
 	<?php include 'includes/analytics.php'; ?>
-	<link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel=" stylesheet">
+	<script src="https://cdn.tailwindcss.com"></script>
 	<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 	<script src="https://unpkg.com/clipboard@2/dist/clipboard.min.js"></script>
 	<script src="icons.min.json"></script>
@@ -25,9 +27,14 @@
 		<div class="flex flex-col items-center justify-center w-full pt-6 md:pt-16">
 			<div class="px-3">
 				<h1 class="pt-6">
-					<span class="px-3 mb-5 text-3xl font-bold text-center text-white bg-brand sm:text-4xl sm:mb-16" style="box-decoration-break: clone;-webkit-box-decoration-break: clone;"><span>Icons for Tailwind CSS</span></span>
+					<span class="px-3 mb-5 text-3xl font-bold text-center text-white bg-brand sm:text-4xl sm:mb-16"
+						style="box-decoration-break: clone;-webkit-box-decoration-break: clone;"><span>Icons for
+							Tailwind CSS</span></span>
 				</h1>
-				<p class="max-w-3xl mx-auto my-6 text-base font-bold leading-normal text-left lg:text-xl lg:text-center">A simple viewer for some Open Source icon packs to customise and copy/paste into your Tailwind CSS project.<br>
+				<p
+					class="max-w-3xl mx-auto my-6 text-base font-bold leading-normal text-left lg:text-xl lg:text-center">
+					A simple viewer for some Open Source icon packs to customise and copy/paste into your Tailwind CSS
+					project.<br>
 			</div>
 		</div>
 	</div>
@@ -38,27 +45,36 @@
 		<div class="w-full px-4 mb-2 md:w-3/5 md:px-3">
 			<div class="sticky w-full mt-6 mb-8 top-icon bg-brand-white z-5">
 
-				<input x-model="search" placeholder="Search for an icon..." type="search" class="block w-full px-4 py-3 font-bold text-gray-700 bg-white rounded-lg focus:outline-none focus:bg-white focus:shadow">
+				<input x-model="search" placeholder="Search for an icon..." type="search"
+					class="block w-full px-4 py-3 font-bold text-gray-700 bg-white rounded-lg focus:outline-none focus:bg-white focus:shadow">
 
 				<div class="mt-2">
-					<a class="tab" href="#" @click.prevent="pack = 'all'" :class="{ 'active-tab' : pack === 'all' }">All</a>
-					<a class="tab" href="#" @click.prevent="pack = 'Heroicons'" :class="{ 'active-tab' : pack === 'Heroicons' }">Heroicons</a>
-					<a class="tab" href="#" @click.prevent="pack = 'Tabler'" :class="{ 'active-tab' : pack === 'Tabler' }">Tabler</a>
-					<a class="tab" href="#" @click.prevent="pack = 'Feather'" :class="{ 'active-tab' : pack === 'Feather' }">Feather</a>
+					<a class="tab" href="#" @click.prevent="pack = 'all'"
+						:class="{ 'active-tab' : pack === 'all' }">All</a>
+					<a class="tab" href="#" @click.prevent="pack = 'Heroicons'"
+						:class="{ 'active-tab' : pack === 'Heroicons' }">Heroicons</a>
+					<a class="tab" href="#" @click.prevent="pack = 'Tabler'"
+						:class="{ 'active-tab' : pack === 'Tabler' }">Tabler</a>
+					<a class="tab" href="#" @click.prevent="pack = 'Feather'"
+						:class="{ 'active-tab' : pack === 'Feather' }">Feather</a>
 				</div>
 
 			</div>
 			<div class="flex flex-wrap">
 
 				<template x-for="icon in filteredIcons" :key="icon.filename">
-					<div x-show="pack === icon.pack || pack === 'all'" class="flex flex-grow-0 p-2 transition duration-150 ease-in-out transform hover:bg-white hover:shadow hover:rounded hover:scale-125 ">
-						<div class="w-8 h-8 mx-auto overflow-hidden" x-html="'<svg class=&quot' + previewClasses + '&quot ' + icon.svg" x-on:click="currentSVG = icon.svg;
+					<div x-show="pack === icon.pack || pack === 'all'"
+						class="flex flex-grow-0 p-2 transition duration-150 ease-in-out transform hover:bg-white hover:shadow hover:rounded hover:scale-125 ">
+						<div class="w-8 h-8 mx-auto overflow-hidden"
+							x-html="'<svg class=&quot' + previewClasses + '&quot ' + icon.svg" x-on:click="currentSVG = icon.svg;
 					
 														$refs.code.value =  needsWeight(classColor) ? 
 															'<svg class=&quot' + classSize + ' ' + classColor + classColorWeight + '&quot ' + icon.svg : 
 															'<svg class=&quot' + classSize + ' ' + classColor +  '&quot ' + icon.svg;
 				
-														$refs.code1.innerHTML  = $refs.code.value;">
+														$refs.code1.innerHTML  = $refs.code.value;
+														
+														currentName = icon.filename.charAt(0).toUpperCase() + icon.filename.slice(1).replace('.svg','')">
 						</div>
 					</div>
 				</template>
@@ -67,18 +83,28 @@
 			</div>
 
 			<p class="pt-8 font-bold">Icon Credits:</p>
-			<a class="font-bold text-brand" href="https://github.com/refactoringui/heroicons" target="_blank" rel="nofollow">Heroicons</a> (<a class="text-brand" href="https://twitter.com/steveschoger" target="_blank" rel="nofollow">Steve Schoger</a>)
-			<a class="font-bold text-brand" href="https://github.com/tabler/tabler-icons" target="_blank" rel="nofollow">Tabler</a> (<a class="text-brand" href="https://twitter.com/codecalm" target="_blank" rel="nofollow">Paweł Kuna</a>) and
-			<a class="font-bold text-brand" href="https://github.com/feathericons/feather" target="_blank" rel="nofollow">Feather</a> (<a class="text-brand" href="https://twitter.com/colebemis" target="_blank" rel="nofollow">Cole Bemis'</a>)
+			<a class="font-bold text-brand" href="https://github.com/refactoringui/heroicons" target="_blank"
+				rel="nofollow">Heroicons</a> (<a class="text-brand" href="https://twitter.com/steveschoger"
+				target="_blank" rel="nofollow">Steve Schoger</a>)
+			<a class="font-bold text-brand" href="https://github.com/tabler/tabler-icons" target="_blank"
+				rel="nofollow">Tabler</a> (<a class="text-brand" href="https://twitter.com/codecalm" target="_blank"
+				rel="nofollow">Paweł Kuna</a>) and
+			<a class="font-bold text-brand" href="https://github.com/feathericons/feather" target="_blank"
+				rel="nofollow">Feather</a> (<a class="text-brand" href="https://twitter.com/colebemis" target="_blank"
+				rel="nofollow">Cole Bemis'</a>)
 
 			<p class="mb-2 font-bold text-center text-brand md:pt-8">Advertisment</p>
-			<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7D52JJ&placement=wwwtailwindtoolboxcom" id="_carbonads_js"></script>
+			<script async type="text/javascript"
+				src="//cdn.carbonads.com/carbon.js?serve=CK7D52JJ&placement=wwwtailwindtoolboxcom"
+				id="_carbonads_js"></script>
 		</div>
-		<div class="sticky z-50 w-full h-full px-2 mx-auto md:w-2/5 top-40 bg-brand-white" x-show.transition="currentSVG != ''">
+		<div class="sticky z-50 w-full h-full px-2 mx-auto md:w-2/5 top-40 bg-brand-white"
+			x-show.transition="currentSVG != ''">
 
 			<button type="button" class="float-right md:hidden" x-on:click="currentSVG = ''">
 
-				<svg class="inline w-8 h-8 text-brand" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<svg class="inline w-8 h-8 text-brand" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+					stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<circle cx="12" cy="12" r="10" />
 					<line x1="15" y1="9" x2="9" y2="15" />
 					<line x1="9" y1="9" x2="15" y2="15" />
@@ -86,10 +112,11 @@
 
 			</button>
 
-			<div class="flex items-center justify-center flex-grow-0 w-full max-w-sm p-2 mx-auto" id="iconPreview" x-ref="code1" x-html="needsWeight(classColor) ? 
+			<div class="flex items-center justify-center p-2 font-semibold text-brand" x-text="currentName"></div>
+			<div class="flex items-center justify-center flex-grow-0 w-full max-w-sm p-2 mx-auto" id="iconPreview"
+				x-ref="code1" x-html="needsWeight(classColor) ? 
 																'<svg class=&quot' + classSize + ' ' + classColor + classColorWeight + '&quot ' + currentSVG : 
 																'<svg class=&quot' + classSize + ' ' + classColor + '&quot ' + currentSVG "></div>
-
 			<div class="flex flex-wrap justify-center w-full">
 
 				<div id="control-textsize" class="mt-4 mb-6">
@@ -135,18 +162,29 @@
 						<select class="block w-full mt-1 form-input" x-model="classColor" x-on:change="$refs.code.value  =  needsWeight(classColor) ? 
 															'<svg class=&quot' + classSize + ' ' + classColor + classColorWeight + '&quot ' + currentSVG : 
 															'<svg class=&quot' + classSize + ' ' + classColor + '&quot ' + currentSVG ">
-							<option value="text-red-" selected>text-red</option>
+							<option value="text-slate-" selected>text-slate</option>
+							<option value="text-gray-">text-gray</option>
+							<option value="text-zinc-">text-zinc</option>
+							<option value="text-neutral-">text-coolGray</option>
+							<option value="text-stone-">text-stone</option>
+							<option value="text-red-">text-red</option>
 							<option value="text-orange-">text-orange</option>
+							<option value="text-amber-">text-amber</option>
 							<option value="text-yellow-">text-yellow</option>
+							<option value="text-lime-">text-lime</option>
 							<option value="text-green-">text-green</option>
+							<option value="text-emerald-">text-emerald</option>
 							<option value="text-teal-">text-teal</option>
+							<option value="text-cyan-">text-cyan</option>
+							<option value="text-sky-">text-sky</option>
 							<option value="text-blue-">text-blue</option>
 							<option value="text-indigo-">text-indigo</option>
+							<option value="text-violet-">text-violet</option>
 							<option value="text-purple-">text-purple</option>
+							<option value="text-fuchsia-">text-fuchsia</option>
 							<option value="text-pink-">text-pink</option>
-							<option value="text-gray-">text-gray </option>
-							<option value="text-black">text-black</option>
-							<option value="text-white">text-white</option>
+							<option value="text-rose-">text-rose</option>
+
 						</select>
 
 						<div class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -156,7 +194,8 @@
 						</div>
 					</div>
 					<div class="flex" x-show="needsWeight(classColor)">
-						<input type="range" min="100" max="900" value="800" step="100" class="mr-2 slider" id="textcolor" x-model="classColorWeight" x-on:change="$refs.code.value  =  needsWeight(classColor) ? 
+						<input type="range" min="100" max="900" value="800" step="100" class="mr-2 slider"
+							id="textcolor" x-model="classColorWeight" x-on:change="$refs.code.value  =  needsWeight(classColor) ? 
 															'<svg class=&quot' + classSize + ' ' + classColor + classColorWeight + '&quot ' + currentSVG : 
 															'<svg class=&quot' + classSize + ' ' + classColor +  '&quot ' + currentSVG " />
 						<span x-text="classColorWeight"></span>
@@ -164,12 +203,17 @@
 				</div>
 			</div>
 
-			<textarea rows="6" readonly class="hidden w-full p-4 border rounded shadow md:block" id="codeSrc" x-ref="code"></textarea>
+			<textarea rows="6" readonly class="hidden w-full p-4 border rounded shadow md:block" id="codeSrc"
+				x-ref="code"></textarea>
 
-			<button type="button" id="clipboardBtn" data-clipboard-target="#codeSrc" class="flex items-center justify-center w-full max-w-xs px-5 py-3 mx-auto mt-0 font-extrabold text-white rounded shadow outline-none md:mt-6 bg-brand hover:bg-teal-500 focus:border-teal-900">
-				<svg class="inline h-4 pr-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+			<button type="button" id="clipboardBtn" data-clipboard-target="#codeSrc"
+				class="flex items-center justify-center w-full max-w-xs px-5 py-3 mx-auto mt-0 font-extrabold text-white rounded shadow outline-none md:mt-6 hover:bg-brand-alt focus:border-teal-900"
+				style="background:#3b7977">
+				<svg class="inline h-4 pr-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+					fill="currentColor">
 					<path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z" />
-					<path d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z" />
+					<path
+						d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z" />
 				</svg>
 				Copy to clipboard
 			</button>
@@ -201,6 +245,7 @@
 				myClasses: ' text-red-800',
 				previewClasses: 'h-8 w-8 fill-stroke text-gray-800 hover:text-blue-500',
 				currentSVG: '',
+				currentName: '',
 				get filteredIcons() {
 					if (this.search === '') {
 						return this.icons
@@ -224,11 +269,11 @@
 		var clipboard = new ClipboardJS('#clipboardBtn');
 
 
-		clipboard.on('success', function(e) {
+		clipboard.on('success', function (e) {
 
 			document.getElementById('clipboardMessage').innerHTML = "Copied!!!!!!";
 
-			setTimeout(function() {
+			setTimeout(function () {
 				document.getElementById('clipboardMessage').innerHTML = "";
 			}.bind(this), 1000);
 
@@ -236,10 +281,10 @@
 			e.clearSelection();
 		});
 
-		clipboard.on('error', function(e) {
+		clipboard.on('error', function (e) {
 			document.getElementById('clipboardMessage').innerHTML = "Something went wrong!";
 
-			setTimeout(function() {
+			setTimeout(function () {
 				document.getElementById('clipboardMessage').innerHTML = "";
 			}.bind(this), 2000);
 
